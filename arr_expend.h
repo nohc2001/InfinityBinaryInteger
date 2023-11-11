7b41284cc31269a73e2e457c5515aaf124bdd789
DrumTimes <nohc2001@naver.com>
2023-11-11 01:04:30 +0000

    save

--- arr_expend.h
+++ arr_expend.h
@@ -173,6 +173,176 @@ class lcstr
 	}
 };
 
+class lwstr
+{
+  public:
+	wchar_t *Arr;
+	size_t maxsize = 0;
+	size_t up = 0;
+	bool islocal = true;
+
+	  lwstr()
+	{
+		Arr = nullptr;
+		maxsize = 0;
+		up = 0;
+		islocal = true;
+	}
+
+	virtual ~ lwstr()
+	{
+		if(islocal){
+			delete[]Arr;
+			Arr = nullptr;
+		}
+	}
+
+	void NULLState()
+	{
+		Arr = nullptr;
+		maxsize = 0;
+		up = 0;
+	}
+
+	void Init(size_t siz, bool local)
+	{
+		islocal = local;
+		wchar_t *newArr = new wchar_t[siz];
+		if (Arr != nullptr)
+		{
+			for (int i = 0; i < maxsize; ++i)
+			{
+				newArr[i] = Arr[i];
+			}
+
+			delete[]Arr;
+			Arr = nullptr;
+		}
+
+		Arr = newArr;
+		maxsize = siz;
+	}
+
+	void operator=(const wchar_t *str)
+	{
+		int len = wcslen(str) + 1;
+		if (Arr == nullptr)
+		{
+			Arr = new wchar_t[len];
+		}
+
+		if (maxsize < len)
+		{
+			Init(len + 1, islocal);
+		}
+
+		wcscpy(Arr, str);
+		up = len - 1;
+	}
+
+	bool operator==(wchar_t *str)
+	{
+		if (wcscmp(Arr, str) == 0)
+			return true;
+		else
+			return false;
+	}
+
+	bool operator==(const wchar_t *str)
+	{
+		if (wcscmp(Arr, str) == 0)
+			return true;
+		else
+			return false;
+	}
+
+	wchar_t &at(size_t i)
+	{
+		return Arr[i];
+	}
+
+	wchar_t *c_str()
+	{
+		Arr[up] = 0;
+		return Arr;
+	}
+
+	wchar_t &operator[] (size_t i)
+	{
+		return Arr[i];
+	}
+
+	void push_back(wchar_t value)
+	{
+		if (up < maxsize)
+		{
+			Arr[up] = value;
+			up += 1;
+			Arr[up] = 0;
+		}
+		else
+		{
+			Init(maxsize * 2 + 1, islocal);
+			Arr[up] = value;
+			up += 1;
+			Arr[up] = 0;
+		}
+	}
+
+	void pop_back()
+	{
+		if (up - 1 >= 0)
+		{
+			up -= 1;
+			Arr[up] = 0;
+		}
+	}
+
+	void erase(size_t i)
+	{
+		for (int k = i; k < up; ++k)
+		{
+			Arr[k] = Arr[k + 1];
+		}
+		up -= 1;
+	}
+
+	void insert(size_t i, wchar_t value)
+	{
+		push_back(value);
+		for (int k = maxsize - 1; k > i; k--)
+		{
+			Arr[k] = Arr[k - 1];
+		}
+		Arr[i] = value;
+	}
+
+	size_t size()
+	{
+		return up;
+	}
+
+	void clear()
+	{
+		if (Arr != nullptr)
+			delete[]Arr;
+		Arr = nullptr;
+		up = 0;
+
+		Init(2, islocal);
+	}
+	
+	void release()
+	{
+		if (Arr != nullptr)
+			delete[]Arr;
+		Arr = nullptr;
+		up = 0;
+		islocal = false;
+	}
+};
+
+
 template < typename T > class vecarr
 {
   public:
