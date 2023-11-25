a65b42a918076633a2791ee1ead1f189943c3d01
DrumTimes <nohc2001@naver.com>
2023-11-25 06:15:12 +0000

    freemem update include arrgraph

--- Utill_FreeMemory.h
+++ Utill_FreeMemory.h
@@ -1976,5 +1976,213 @@ namespace freemem{
 			--size;
 		}
 	};
+
+	typedef struct VP
+	{
+		char mod = 0;			// mod 0:value 1:ptr
+		int *ptr = nullptr;		// arrgraph ptr or T ptr
+	};
+
+	template < typename T, typename V > struct range
+	{
+		T end;
+		V value;
+	};
+
+	template < typename T, typename V > class ArrGraph
+	{
+	  public:
+		fmvecarr < range < T, V > >* ranges;
+		T minx = 0;
+		T maxx = 0;
+		T margin = 0;
+		bool islocal = false;
+		bool isdebug = true;
+		fmvecarr < VP > graph;
+
+		ArrGraph()
+		{
+		}
+		virtual ~ ArrGraph()
+		{
+			if (islocal)
+			{
+				if (fm->bAlloc(reinterpret_cast < byte8 * >(ranges),
+						   sizeof(vecarr < range < T, V > >)))
+				{
+					ranges->release();
+					fm->_Delete((byte8 *) ranges, sizeof(fmvecarr < range < T, V > >));
+					ranges = nullptr;
+				}
+
+				graph.release();
+				graph.NULLState();
+			}
+		}
+
+		ArrGraph *Init(T min, T max, FM_System0 * _fm)
+		{
+			minx = min;
+			maxx = max;
+			fm = _fm;
+			ranges = (fmvecarr < range < T, V > >*)fm->_New(sizeof(fmvecarr < range < T, V > >), true);
+			ranges->NULLState();
+			ranges->Init(2, false, true);
+			islocal = false;
+		}
+
+		range < T, V > Range(T end, V value)
+		{
+			range < T, V > r;
+			r.end = end;
+			r.value = value;
+			return r;
+		}
+
+		void push_range(range < T, V > r)
+		{
+			if (minx <= r.end && r.end <= maxx)
+			{
+				ranges->push_back(r);
+			}
+		}
+
+		void Compile()
+		{
+			if (ranges->size() > 2)
+			{
+				float d = (float)(maxx - minx);
+				float div = (float)ranges->size();
+				float f = d / div;
+				f = floor(f) + 1;
+				T average_length = (T) (f);
+				margin = average_length;
+				graph.NULLState();
+				graph.Init(ranges->size(), false, true);
+				graph.up = ranges->size();
+				T start = minx;
+				T end = start;
+				for (int i = 0; i < graph.up; ++i)
+				{
+					end = start + average_length;
+					if (end > maxx)
+						end = maxx;
+					T rstart = minx;
+					for (int k = 0; k < ranges->up; ++k)
+					{
+						T rend = ranges->at(k).end;
+						if (rstart <= start && end <= rend)
+						{
+							// num
+							graph[i].mod = 0;
+							graph[i].ptr = reinterpret_cast < int *>(&ranges->at(k).value);
+							break;
+						}
+						else if (start <= rend && rend <= end)
+						{
+							// graph
+							ArrGraph < T, V > *newgraph =
+								(ArrGraph < T, V > *)fm->_New(sizeof(ArrGraph < T, V >), true);
+							newgraph->Init(start, end, fm);
+							newgraph->push_range(ranges->at(k));
+							range < T, V > *r = &ranges->at(k + 1);
+							while (r->end <= end)
+							{
+								newgraph->push_range(*r);
+								++k;
+								if (k >= ranges->size())
+								{
+									break;
+								}
+								r = &ranges->at(k + 1);
+							}
+							// input last range
+							range < T, V > lastr;
+							lastr = *r;
+							lastr.end = newgraph->maxx;
+							newgraph->push_range(lastr);
+							newgraph->Compile();
+							graph[i].ptr = reinterpret_cast < int *>(newgraph);
+							graph[i].mod = 1;
+							break;
+						}
+					}
+					start = end;
+				}
+			}
+			else if (ranges->size() == 2)
+			{
+				graph.NULLState();
+				graph.Init(2, false, true);
+				T center = ranges->at(0).end;
+				T start = minx;
+				T end = maxx - 1;
+				if (maxx - center > center - start)
+				{
+					minx = 2 * center + 1 - end;
+				}
+				else
+				{
+					maxx = 2 * center + 1 - start;
+				}
+				margin = (maxx - minx) / ranges->size();
+				VP vp0;
+				vp0.mod = 0;
+				vp0.ptr = reinterpret_cast < int *>(&ranges->at(0).value);
+				graph.push_back(vp0);
+				vp0.ptr = reinterpret_cast < int *>(&ranges->at(1).value);
+				graph.push_back(vp0);
+			}
+		}
+
+		V fx(T x)
+		{
+			static constexpr void *jumpptr[2] = { &&ISVALUE, &&ISGRAPH };
+			ArrGraph < T, V > *ag = this;
+			fmvecarr < VP > *g = &graph;
+			VP vp;
+			float f = 0;
+			int index = 0;
+
+		  GET_START:
+			f = (float)x - (float)ag->minx;
+			f = f / (float)ag->margin;
+			index = (int)f;
+
+			vp = (*g)[index];
+			goto *jumpptr[vp.mod];
+
+		  ISGRAPH:
+			ag = reinterpret_cast < ArrGraph < T, V > *>(vp.ptr);
+			g = &ag->graph;
+			goto GET_START;
+
+		  ISVALUE:
+			return *reinterpret_cast < V * >(vp.ptr);
+		}
+
+		void print_state()
+		{
+			cout << "arrgraph" << endl;
+			cout << "minx : " << minx << endl;
+			cout << "maxx : " << maxx << endl;
+			cout << "capacity : " << graph.size() << endl;
+			cout << "margin : " << margin << endl;
+			for (int i = 0; i < graph.size(); ++i)
+			{
+				if (graph[i].mod == 0)
+				{
+					cout << "index : " << i << "] = value : " << *reinterpret_cast <
+						V * >(graph[i].ptr) << endl;
+				}
+				else
+				{
+					cout << "index : " << i << "] = ptr : " << endl;
+					reinterpret_cast < ArrGraph < T, V > *>(graph[i].ptr)->print_state();
+					cout << endl;
+				}
+			}
+		}
+	};
 }
 #endif
